b583cd4ea96e9dded58081d489ffd9eb5cbfc6fd
vitor barbosa de lima santos <visantos@sbt.com.br>
2021-07-02 14:48:59 -0300

    initial commit

---
+++ components/header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { Container } from '@material-ui/core'
+import { Div } from './style'
+
+const Header = () => (
+  <Container>
+    <Div>teste</Div>
+  </Container>
+)
+
+export default Header
